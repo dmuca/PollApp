09dc82659f8ac592d1f4f5a55c0a27effbae204b
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-05-28 22:30:42 +0200

    Create Log In template.

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserListComponent} from './view/user-list/user-list.component';
 import { UserRegisterComponent} from './view/user-register/user-register.component';
+import {UserLogInComponent} from './view/user-log-in/user-log-in.component';
 
 const routes: Routes = [
   { path: 'users', component: UserListComponent },
   { path: 'registerUser', component: UserRegisterComponent },
+  { path: 'login', component: UserLogInComponent },
 ];
 
 @NgModule({
