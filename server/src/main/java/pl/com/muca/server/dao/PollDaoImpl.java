674ff5a0a97edc575f5f438ef83cc61737f77122
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-06-10 19:47:17 +0200

    Read state for each poll (New or Filled).

--- server/src/main/java/pl/com/muca/server/dao/PollDaoImpl.java
+++ server/src/main/java/pl/com/muca/server/dao/PollDaoImpl.java
@@ -1,9 +1,13 @@
 package pl.com.muca.server.dao;
 
+import static pl.com.muca.server.entity.PollState.Filled;
+import static pl.com.muca.server.entity.PollState.New;
+
 import com.google.common.collect.ImmutableList;
 import java.sql.PreparedStatement;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 import org.springframework.jdbc.core.PreparedStatementCallback;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
@@ -12,15 +16,14 @@ import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 import pl.com.muca.server.entity.Poll;
+import pl.com.muca.server.entity.PollState;
 import pl.com.muca.server.mapper.PollRowMapper;
 
 @Repository
 public class PollDaoImpl implements PollDao {
 
   private static final String UPDATE_SQL =
-      "UPDATE poll "
-          + "SET name=:name, owner_user_id=:owner_user_id "
-          + "WHERE poll_id=:poll_id";
+      "UPDATE poll " + "SET name=:name, owner_user_id=:owner_user_id " + "WHERE poll_id=:poll_id";
 
   private final NamedParameterJdbcTemplate template;
 
@@ -30,30 +33,55 @@ public class PollDaoImpl implements PollDao {
 
   @Override
   public ImmutableList<Poll> findAll() {
-    return ImmutableList
-        .copyOf(template.query("SELECT * FROM poll", new PollRowMapper()));
+    ImmutableList<Poll> polls =
+        ImmutableList.copyOf(template.query("SELECT * FROM poll", new PollRowMapper()));
+
+    polls.forEach(poll -> poll.setState(getPollState(poll.getPollId())));
+    return polls;
+  }
+
+  private PollState getPollState(int pollId) {
+    // TODO use user session to determine user_id parameter.
+    int userId = 3;
+
+    String countUserAnswersToPollSql =
+        "SELECT COUNT(*) AS howManyAnswers "
+            + "FROM useranswer "
+            + "INNER JOIN question "
+            + "ON useranswer.question_id = question.question_id "
+            + "WHERE user_id=:UserId AND question.poll_id=:PollId";
+
+    SqlParameterSource namedParameters =
+        new MapSqlParameterSource().addValue("PollId", pollId).addValue("UserId", userId);
+    Optional<Integer> answersToPoll =
+        Optional.ofNullable(
+            template.queryForObject(countUserAnswersToPollSql, namedParameters, Integer.class));
+    return answersToPoll.filter(integer -> integer > 0).map(integer -> Filled).orElse(New);
   }
 
   @Override
   public void insertPoll(Poll poll) {
-    final String sql = "INSERT INTO poll(poll_id, owner_user_id, name) "
-        + "VALUES (:poll_id, :owner_user_id, :name)";
+    final String sql =
+        "INSERT INTO poll(poll_id, owner_user_id, name) "
+            + "VALUES (:poll_id, :owner_user_id, :name)";
 
     KeyHolder holder = new GeneratedKeyHolder();
-    SqlParameterSource param = new MapSqlParameterSource()
-        .addValue("poll_id", poll.getPollId())
-        .addValue("owner_user_id", poll.getOwnerUserId())
-        .addValue("name", poll.getName().trim());
+    SqlParameterSource param =
+        new MapSqlParameterSource()
+            .addValue("poll_id", poll.getPollId())
+            .addValue("owner_user_id", poll.getOwnerUserId())
+            .addValue("name", poll.getName().trim());
     template.update(sql, param, holder);
   }
 
   @Override
   public void updatePoll(Poll poll) {
     KeyHolder holder = new GeneratedKeyHolder();
-    SqlParameterSource param = new MapSqlParameterSource()
-        .addValue("poll_id", poll.getPollId())
-        .addValue("owner_user_id", poll.getOwnerUserId())
-        .addValue("name", poll.getName().trim());
+    SqlParameterSource param =
+        new MapSqlParameterSource()
+            .addValue("poll_id", poll.getPollId())
+            .addValue("owner_user_id", poll.getOwnerUserId())
+            .addValue("name", poll.getName().trim());
     template.update(UPDATE_SQL, param, holder);
   }
 
@@ -64,8 +92,8 @@ public class PollDaoImpl implements PollDao {
     map.put("owner_user_id", poll.getOwnerUserId());
     map.put("name", poll.getName().trim());
 
-    template.execute(UPDATE_SQL, map,
-        (PreparedStatementCallback<Object>) PreparedStatement::executeUpdate);
+    template.execute(
+        UPDATE_SQL, map, (PreparedStatementCallback<Object>) PreparedStatement::executeUpdate);
   }
 
   @Override
@@ -73,7 +101,7 @@ public class PollDaoImpl implements PollDao {
     final String sql = "DELETE FROM poll WHERE poll_id=:poll_id";
     final Map<String, Object> map = new HashMap<>();
     map.put("poll_id", poll.getPollId());
-    template.execute(sql, map,
-        (PreparedStatementCallback<Object>) PreparedStatement::executeUpdate);
+    template.execute(
+        sql, map, (PreparedStatementCallback<Object>) PreparedStatement::executeUpdate);
   }
 }
