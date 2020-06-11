4aa223570e124411b9e8e63bfbad16f2b0bea91b
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-06-11 14:21:29 +0200

    Fix REST API URL requests.

--- web/src/app/model/authentication/authentication.service.ts
+++ web/src/app/model/authentication/authentication.service.ts
@@ -1,18 +1,20 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 import {User} from '../user/user';
 
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class AuthenticationService {
   private currentUserSubject: BehaviorSubject<User>;
   public currentUser: Observable<User>;
+  private url: string;
 
   constructor(private http: HttpClient) {
     this.currentUserSubject = new BehaviorSubject<User>(JSON.parse(localStorage.getItem('currentUser')));
     this.currentUser = this.currentUserSubject.asObservable();
+    this.url = 'http://93.180.178.64:2000/pollApp/';
   }
 
   public get currentUserValue(): User {
@@ -21,9 +23,11 @@ export class AuthenticationService {
 
   login(username, password) {
     // TODO (Damian Muca): 6/11/20 configure config.apiUrl global constant.
-    // return this.http.post<any>(`${config.apiUrl}/users/authenticate`, { username, password })
-    return this.http.post<any>(`http://93.180.178.64:2000/pollApp/users/authenticate`, { username, password })
+    // return this.http.post<any>(`${config.apiUrl}login`, { username, password })
+    return this.http.post<User>(`${this.url}login`, {username, password})
     .pipe(map(user => {
+      console.log('User login');
+      console.log(user);
       localStorage.setItem('currentUser', JSON.stringify(user));
       this.currentUserSubject.next(user);
       return user;
