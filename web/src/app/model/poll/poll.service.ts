9e0c502a771d386b869765191a3605edf1cc3aa8
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-06-11 18:46:37 +0200

    Remove fake polls data.

--- web/src/app/model/poll/poll.service.ts
+++ web/src/app/model/poll/poll.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {Poll} from './poll';
 import {Subject} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
-import {PollState} from './poll.state';
 import {REST_API_URL} from '../../../common';
 
 @Injectable({
@@ -15,18 +14,8 @@ export class PollService {
   }
 
   public listAllPolls() {
-    const fakePolls: Poll[] = [{
-      pollId: 5,
-      name: 'Ankieta 1',
-      state: PollState.Filled,
-    }, {
-      pollId: 6,
-      name: 'Ankieta 2',
-      state: PollState.New,
-    }];
-    this.pollsList$.next(fakePolls);
     this.http.get<Poll[]>(`${REST_API_URL}listPolls`).subscribe((polls) => {
-      this.pollsList$.next(polls.concat(fakePolls));
+      this.pollsList$.next(polls);
     });
   }
 }
