aa72cb879376fa51df7a95764f696e783f59b697
Tomasz <tiksmodiw@gmail.com>
2020-06-11 11:40:03 +0200

    Add auth guard and alert component.

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { UserListComponent} from './view/user-list/user-list.component';
-import { RegisterComponent} from './view/register/register.component';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {UserListComponent} from './view/user-list/user-list.component';
+import {RegisterComponent} from './view/register/register.component';
 import {LoginComponent} from './view/login/login.component';
 import {AllPollsComponent} from './view/polls/all-polls.component';
 import {CreatePollComponent} from './view/create-poll/create-poll.component';
 import {MyPollsComponent} from './view/my-polls/my-polls.component';
+import {AuthGuard} from './model/authentication/auth.guard';
 
 const routes: Routes = [
-  { path: 'users', component: UserListComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: 'myPolls', component: MyPollsComponent },
-  { path: 'allPolls', component: AllPollsComponent },
-  { path: 'createPoll', component: CreatePollComponent },
+  {path: '', redirectTo: 'users', pathMatch: 'full'},
+  {path: 'users', component: UserListComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'myPolls', component: MyPollsComponent, canActivate: [AuthGuard]},
+  {path: 'allPolls', component: AllPollsComponent, canActivate: [AuthGuard]},
+  {path: 'createPoll', component: CreatePollComponent, canActivate: [AuthGuard]},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
