34aeea4a67675fb47ca927f38c41ca3171dcb90d
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-06-11 18:59:13 +0200

    Fix Authorization token.

--- web/src/app/model/interceptors/jwt.interceptor.ts
+++ web/src/app/model/interceptors/jwt.interceptor.ts
@@ -1,19 +1,25 @@
-import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import {Observable} from 'rxjs';
 import {AuthenticationService} from '../authentication/authentication.service';
 
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(private authenticationService: AuthenticationService) {
+  }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const currentUser = this.authenticationService.currentUserValue;
     if (currentUser && currentUser.token) {
       request = request.clone({
         setHeaders: {
-          Authorization: `Bearer ${currentUser.token}`
+          Authorization: `${currentUser.token}`
         }
       });
     }
