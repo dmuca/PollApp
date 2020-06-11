a2c116f369e2326ef3aada2fcc6271d7b923bed3
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-06-11 18:49:15 +0200

    Add second poll to sample database data.

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -34,3 +34,36 @@ INSERT INTO answer(answer_id, question_id, content) VALUES (13, 3, 'To był miss
 INSERT INTO useranswer(user_id, question_id, answer_chosen) VALUES (1, 3, 11);
 INSERT INTO useranswer(user_id, question_id, answer_chosen) VALUES (2, 3, 9);
 INSERT INTO useranswer(user_id, question_id, answer_chosen) VALUES (3, 3, 12);
+
+-- Poll nr.2
+INSERT INTO poll(poll_id, owner_user_id, name) VALUES (2, 2, 'Moje miasto - Kraków');
+-- Question nr.1
+INSERT INTO question(question_id, poll_id, content) VALUES (4, 2, 'Co myślisz o swoim mieście?');
+INSERT INTO answer(answer_id, question_id, content) VALUES (14, 4, 'Super');
+INSERT INTO answer(answer_id, question_id, content) VALUES (15, 4, 'Odlotowe');
+INSERT INTO answer(answer_id, question_id, content) VALUES (16, 4, 'Świetne');
+INSERT INTO answer(answer_id, question_id, content) VALUES (17, 4, 'Najlepsze');
+INSERT INTO answer(answer_id, question_id, content) VALUES (18, 4, 'Niesamowite');
+INSERT INTO useranswer(user_id, question_id, answer_chosen) VALUES (4, 4, 13);
+-- Question nr.2
+INSERT INTO question(question_id, poll_id, content) VALUES (5, 2, 'Czy poleciłbyś Kraków znajomym?');
+INSERT INTO answer(answer_id, question_id, content) VALUES (19, 5, 'Tak');
+INSERT INTO answer(answer_id, question_id, content) VALUES (20, 5, 'Owszem');
+INSERT INTO answer(answer_id, question_id, content) VALUES (21, 5, 'Polecę');
+INSERT INTO useranswer(user_id, question_id, answer_chosen) VALUES (4, 5, 19);
+
+-- CLEARING TABLES
+DELETE FROM appuser WHERE TRUE=TRUE;
+DELETE FROM poll WHERE TRUE=TRUE;
+DELETE FROM question WHERE TRUE=TRUE;
+DELETE FROM answer WHERE TRUE=TRUE;
+DELETE FROM useranswer WHERE TRUE=TRUE;
+DELETE FROM session WHERE TRUE=TRUE;
+
+-- DROP ALL TABLES
+DROP TABLE useranswer;
+DROP TABLE answer;
+DROP TABLE question;
+DROP TABLE poll;
+DROP TABLE appuser;
+DROP TABLE session;
