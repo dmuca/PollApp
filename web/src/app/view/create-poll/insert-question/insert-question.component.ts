151012b2ee3978180576c2e64c395b4e68786f89
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-06-11 21:29:23 +0200

    Add new layout for add question input.

--- web/src/app/view/create-poll/insert-question/insert-question.component.ts
+++ web/src/app/view/create-poll/insert-question/insert-question.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Question} from '../../../model/poll/question';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {AlertService} from '../../../model/alert/alert.service';
 
 @Component({
   selector: 'app-insert-question',
@@ -10,7 +12,8 @@ export class InsertQuestionComponent implements OnInit {
   @Input()
   question: Question;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
