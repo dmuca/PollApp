0f3cce6fb8483952515daaea0a4fa58a262de510
Tomasz <tiksmodiw@gmail.com>
2020-06-11 14:10:40 +0200

    Fix displaying message when login failed.

--- web/src/app/view/login/login.component.ts
+++ web/src/app/view/login/login.component.ts
@@ -68,7 +68,7 @@ export class LoginComponent implements OnInit {
         this.router.navigate([this.returnUrl]);
       },
       error => {
-        this.alertService.error(error);
+        this.alertService.error(error.error.message);
         this.loading = false;
       });
   }
