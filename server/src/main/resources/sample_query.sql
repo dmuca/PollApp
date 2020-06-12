c38946101aa0d6ee2b60ef9d74c32f23c34bb453
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-06-13 00:12:12 +0200

    User can check answers given to his poll

--- server/src/main/resources/sample_query.sql
+++ server/src/main/resources/sample_query.sql
@@ -1,18 +1,4 @@
-SELECT COUNT(*) AS markedCounter
-FROM useranswer
-INNER JOIN answer
-ON useranswer.answer_chosen = :AnswerId
-WHERE useranswer.user_id_hash = :UserId AND useranswer.question_id;
-
-SELECT COUNT(*) AS markedCounter
-FROM useranswer
-INNER JOIN answer
-ON useranswer.answer_chosen = :AnswerId
-WHERE useranswer.user_id_hash = :UserId;
-
-
-SELECT COUNT(*) AS markedCounter
-FROM useranswer
-INNER JOIN answer
-ON useranswer.answer_chosen = 2
-WHERE useranswer.user_id_hash = 2;
+SELECT COUNT(*) FROM question
+INNER JOIN poll
+ON poll.poll_id = question.poll_id
+WHERE question.question_id = :QuestionId AND poll.owner_user_id = :UserId;
