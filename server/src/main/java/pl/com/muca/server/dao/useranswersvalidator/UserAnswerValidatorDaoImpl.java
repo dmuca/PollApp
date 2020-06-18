5661ac9ef65dd5a40c2bfa361fc5e9b99fbcff84
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-06-19 00:56:06 +0200

    Add loging info about answers validation hash code generation.

--- server/src/main/java/pl/com/muca/server/dao/useranswersvalidator/UserAnswerValidatorDaoImpl.java
+++ server/src/main/java/pl/com/muca/server/dao/useranswersvalidator/UserAnswerValidatorDaoImpl.java
@@ -1,5 +1,7 @@
 package pl.com.muca.server.dao.useranswersvalidator;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
@@ -14,8 +16,7 @@ public class UserAnswerValidatorDaoImpl implements UserAnswerValidatorDao {
   }
 
   @Override
-  public void insertToUserAnswerValidator(int userId, int pollId,
-      int validationHashCode) {
+  public void insertToUserAnswerValidator(int userId, int pollId, int validationHashCode) {
     final String insertToUserAnswerValidatorSql =
         "INSERT INTO useranswersvalidator(user_id, poll_id, validation_hash_code) "
             + "VALUES (:UserId, :PollId, :ValidationHashCode)";
@@ -24,6 +25,10 @@ public class UserAnswerValidatorDaoImpl implements UserAnswerValidatorDao {
             .addValue("UserId", userId)
             .addValue("PollId", pollId)
             .addValue("ValidationHashCode", validationHashCode);
+    String time = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now());
+    System.out.printf(
+        "%s User with id: %d, answered on poll id: %d, generated validation hash code: %d\n",
+        time, userId, pollId, validationHashCode);
     template.update(insertToUserAnswerValidatorSql, parameterSource);
   }
 }
