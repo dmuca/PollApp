cf6d1e0510be4503668014b2f7b9cce9dd3b3ea5
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-06-19 23:20:19 +0200

    Add view of users who answered/didn't answered a poll.

--- web/src/app/model/poll/poll.service.ts
+++ web/src/app/model/poll/poll.service.ts
@@ -5,6 +5,7 @@ import {HttpClient} from '@angular/common/http';
 import {REST_API_URL} from '../../../common';
 import {UserAnswer} from './user.answer';
 import {UserAnswerValidator} from './user.answer.validator';
+import {User} from '../user/user';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,8 @@ export class PollService {
   public pollsList$: Subject<Poll[]> = new Subject<Poll[]>();
   public myPollsList$: Subject<Poll[]> = new Subject<Poll[]>();
   public pollDetails$: Subject<Poll> = new Subject<Poll>();
+  public usersWhoAnsweredPoll$: Subject<User[]> = new Subject<User[]>();
+  public usersWhoDidNotAnswerPoll$: Subject<User[]> = new Subject<User[]>();
 
   constructor(private http: HttpClient) {
   }
@@ -45,5 +48,47 @@ export class PollService {
 
   public verifyPollAnswers(userAnswerValidator: UserAnswerValidator): Observable<boolean> {
     return this.http.post<boolean>(`${REST_API_URL}verifyPollAnswers`, userAnswerValidator);
+  }
+
+  getUsersWhoAnsweredToPoll(pollId: number) {
+    const users: User[] = [{
+      id: 0,
+      firstName: 'Kasia',
+      lastName: 'Kowalska',
+      password: 'unknown',
+      email: 'Kasia@wp.pl',
+      token: 'unknown',
+    }, {
+      id: 1,
+      firstName: 'Bartosz',
+      lastName: 'Pliżga',
+      password: 'unknown',
+      email: 'BP@wp.pl',
+      token: 'unknown',
+    }
+    ];
+    console.log(users);
+    this.usersWhoAnsweredPoll$.next(users);
+  }
+
+  getUsersWhoDidNotAnswerToPoll(pollId: number) {
+    const users: User[] = [{
+      id: 2,
+      firstName: 'Aleks',
+      lastName: 'Maslana',
+      password: 'unknown',
+      email: 'AKKKK@wp.pl',
+      token: 'unknown',
+    }, {
+      id: 3,
+      firstName: 'Konieczko',
+      lastName: 'Konieczny',
+      password: 'unknown',
+      email: 'koniecznie@wp.pl',
+      token: 'unknown',
+    }
+    ];
+    console.log(users);
+    this.usersWhoDidNotAnswerPoll$.next(users);
   }
 }
