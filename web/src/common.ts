ce2d014c9f3d0ac03a752cf32c1815828df316fe
Damian Muca <DamianAndrzejMuca@gmail.com>
2020-06-20 14:08:57 +0200

    Change API REST address to prod env.

--- web/src/common.ts
+++ web/src/common.ts
@@ -1,3 +1,1 @@
-// TODO (Damian Muca): 6/18/20 Uncomment.
-// export const REST_API_URL = 'http://93.180.178.64:2000/pollApp/';
-export const REST_API_URL = 'http://192.168.0.4:8080/pollApp/';
+export const REST_API_URL = 'http://93.180.178.64:2000/pollApp/';
